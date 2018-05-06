4f3e05005ec11e654d5edaa4f320b1ce240d9fdc
azu <azuciao@gmail.com>
2018-05-06 23:22:46 +0900

    fix(Entity): remove unused function

--- src/EntityLike.ts
+++ src/EntityLike.ts
@@ -1,12 +1,4 @@
-import { ReadonlyEntity } from "./immutable/ImmutableEntity";
 import { Identifier } from "./Identifier";
-
-export const isEntityLike = (entity: any): entity is EntityLike<any> => {
-    if (!entity) {
-        return false;
-    }
-    return entity instanceof ReadonlyEntity;
-};
 
 export interface EntityLikeProps<Id extends Identifier<any>> {
     id: Id;
