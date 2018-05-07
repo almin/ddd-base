c17a11437fda67a5f988d22c8c07d500514c8184
azu <azuciao@gmail.com>
2018-05-07 21:28:46 +0900

    feat(mixin): add Copyable

---
+++ src/mixin/Copyable.ts
@@ -0,0 +1,69 @@
+import { Identifier } from "./../Identifier";
+import { EntityLikeProps } from "./../EntityLike";
+import { EntityLike } from "../EntityLike";
+
+/**
+ * Constructor type
+ */
+export type Constructor<T = {}> = new (...args: any[]) => T;
+
+/**
+ *
+ * K is type of a key
+ * T[K] is type of its value
+ */
+export type PartialMap<T> = { [K in keyof T]?: (prev: T[K]) => T[K] };
+
+/**
+ * Mixin Copyable to the BaseClass
+ * @param BaseClass
+ * @returns Copyable class
+ * @example
+ *
+ * class A extends Copyable<Entity<EntityIdentifier>>{}
+ *
+ * @see https://github.com/Microsoft/TypeScript/issues/22431#issuecomment-371908767
+ */
+export const Copyable = <
+    K extends Constructor<EntityLike<EntityLikeProps<Identifier<any>>>>,
+    E extends InstanceType<K>,
+    Props extends E["props"]
+>(
+    BaseClass: K
+) => {
+    return class extends BaseClass {
+        /**
+         * Return partial change of this object
+         *
+         * e.g)
+         * `new Person("jack", 2).copy({age: 10})` is  `new Person("jack", 10)`
+         *
+         */
+        copy(partial: Partial<Props>): this {
+            const newProps = Object.assign(this.props, partial);
+            const Construct = this.constructor as any;
+            return new Construct(newProps) as this;
+        }
+
+        /**
+         * Return partial change of this object by using functions
+         *
+         * e.g)
+         * `new Person("jack", 10).mapCopy({age: prev => prev+1})` is  `new Person("jack", 11)`
+         *
+         */
+        mapCopy(partial: PartialMap<Props>): this {
+            const newProps: { [index: string]: any } = {};
+            const oldInstance: { [index: string]: any } = this;
+            Object.keys(this.props).forEach(key => {
+                if (key in partial) {
+                    newProps[key] = (partial as any)[key](oldInstance[key]);
+                } else {
+                    newProps[key] = oldInstance[key];
+                }
+            });
+            const Construct = this.constructor as any;
+            return new Construct(newProps) as this;
+        }
+    };
+};
---
+++ test/Copyable-test.ts
@@ -0,0 +1,106 @@
+// Entity A
+import { Entity, Identifier } from "../src";
+import { Copyable } from "../src/mixin/Copyable";
+import * as assert from "assert";
+
+describe("Copyable", () => {
+    it("should inherit Mixin CLass", () => {
+        class AIdentifier extends Identifier<string> {}
+
+        interface AProps {
+            id: AIdentifier;
+            value: string;
+        }
+
+        class AEntity extends Entity<AProps> {
+            rootValue: string = "rootValue";
+        }
+
+        const aEntity = new AEntity({
+            id: new AIdentifier("a"),
+            value: "a"
+        });
+        assert.ok(aEntity instanceof AEntity);
+        assert.strictEqual(aEntity.props.value, "a");
+        assert.strictEqual(aEntity.rootValue, "rootValue");
+
+        class CopyableEntity extends Copyable(AEntity) {
+            cProps = "c";
+        }
+
+        const entity = new CopyableEntity({
+            id: new AIdentifier("test"),
+            value: "string"
+        });
+        assert.ok(entity instanceof CopyableEntity);
+        assert.strictEqual(entity.props.value, "string");
+        assert.strictEqual(entity.cProps, "c");
+        assert.strictEqual(entity.rootValue, "rootValue");
+        const newEntity = entity.copy({
+            value: "new"
+        });
+        assert.ok(newEntity instanceof CopyableEntity);
+        assert.strictEqual(newEntity.props.value, "new");
+        assert.strictEqual(newEntity.rootValue, "rootValue");
+    });
+    describe("#copy", () => {
+        it("should copy prop partially", () => {
+            class AIdentifier extends Identifier<string> {}
+
+            interface AProps {
+                id: AIdentifier;
+                value: string;
+            }
+
+            class AEntity extends Entity<AProps> {}
+
+            class CopyableEntity extends Copyable(AEntity) {}
+
+            const entity = new CopyableEntity({
+                id: new AIdentifier("test"),
+                value: "string"
+            });
+            const newEntity = entity.copy({
+                value: "new"
+            });
+            assert.strictEqual(newEntity.props.id, entity.props.id);
+            assert.strictEqual(newEntity.props.value, "new");
+        });
+    });
+    describe("#mapCopy", () => {
+        it("should map and copy", () => {
+            it("should copy prop partially", () => {
+                class AIdentifier extends Identifier<string> {}
+
+                interface AProps {
+                    id: AIdentifier;
+                    value: string;
+                }
+
+                class AEntity extends Entity<AProps> {}
+
+                class CopyableEntity extends Copyable(AEntity) {}
+
+                const entity = new CopyableEntity({
+                    id: new AIdentifier("test"),
+                    value: "string"
+                });
+                const newEntity = entity
+                    .mapCopy({
+                        value: prevValue => {
+                            assert.strictEqual(prevValue, "value");
+                            return "a";
+                        }
+                    })
+                    .mapCopy({
+                        value: prevValue => {
+                            assert.strictEqual(prevValue, "a");
+                            return "b";
+                        }
+                    });
+                assert.strictEqual(newEntity.props.id, entity.props.id);
+                assert.strictEqual(newEntity.props.value, "b");
+            });
+        });
+    });
+});
