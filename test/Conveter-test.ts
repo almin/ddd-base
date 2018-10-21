1962639a69c1685d7f8a53edcab82b4ba415d397
azu <azuciao@gmail.com>
2018-10-21 18:19:18 +0900

    feat(Converter): support nesting Converter

--- test/Conveter-test.ts
+++ test/Conveter-test.ts
@@ -6,100 +6,148 @@ class AIdentifier extends Identifier<string> {}
 
 interface AProps {
     id: AIdentifier;
-    a: number;
-    b: string;
+    a1: number;
+    a2: string;
 }
 
 class AEntity extends Entity<AProps> {
     constructor(args: AProps) {
         super(args);
     }
-
-    toJSON(): AEntityJSON {
-        return {
-            id: this.props.id.toValue(),
-            a: this.props.a,
-            b: this.props.b
-        };
-    }
 }
 
 interface AEntityJSON {
     id: string;
-    a: number;
-    b: string;
+    a1: number;
+    a2: string;
 }
 
+const AConverter = createConverter<AProps, AEntityJSON>(AEntity, {
+    id: [prop => prop.toValue(), json => new AIdentifier(json)],
+    a1: [prop => prop, json => json],
+    a2: [prop => prop, json => json]
+});
+
 // ValueObject B
-// Entity A
 interface BValueProps {
-    a: number;
-    b: string;
+    b1: number;
+    b2: string;
 }
 
 class BValue extends ValueObject<BValueProps> {}
 
 interface BValueJSON {
-    a: number;
-    b: string;
+    b1: number;
+    b2: string;
 }
+
+const BConverter = createConverter<BValueProps, BValueJSON>(BValue, {
+    b1: [prop => prop, json => json],
+    b2: [prop => prop, json => json]
+});
+
+// Parent Of A, B
+class ParentIdentifier extends Identifier<string> {}
+
+interface ParentJSON {
+    id: string;
+    a: AEntityJSON;
+    b: BValueJSON;
+}
+
+interface ParentProps {
+    id: ParentIdentifier;
+    a: AEntity;
+    b: BValue;
+}
+
+class ParentEntity extends Entity<ParentProps> {}
 
 describe("Converter", function() {
     it("should convert JSON <-> Entity", () => {
         const converter = createConverter<AProps, AEntityJSON>(AEntity, {
             id: [prop => prop.toValue(), json => new AIdentifier(json)],
-            a: [prop => prop, json => json],
-            b: [prop => prop, json => json]
+            a1: [prop => prop, json => json],
+            a2: [prop => prop, json => json]
         });
         const entity = new AEntity({
             id: new AIdentifier("a"),
-            a: 42,
-            b: "b prop"
+            a1: 42,
+            a2: "b prop"
         });
         const json = converter.toJSON(entity);
         assert.deepStrictEqual(json, {
             id: "a",
-            a: 42,
-            b: "b prop"
+            a1: 42,
+            a2: "b prop"
         });
         const entity2 = converter.fromJSON(json);
         assert.deepStrictEqual(entity, entity2);
     });
     it("should convert Props <-> Entity", () => {
-        const converter = createConverter<AProps, AEntityJSON>(AEntity, {
-            id: [prop => prop.toValue(), json => new AIdentifier(json)],
-            a: [prop => prop, json => json],
-            b: [prop => prop, json => json]
-        });
         const entity = new AEntity({
             id: new AIdentifier("a"),
-            a: 42,
-            b: "b prop"
+            a1: 42,
+            a2: "b prop"
         });
-        const json = converter.toJSON(entity);
+        const json = AConverter.toJSON(entity);
         assert.deepStrictEqual(json, {
             id: "a",
-            a: 42,
-            b: "b prop"
+            a1: 42,
+            a2: "b prop"
         });
-        const props = converter.JSONToProps(json);
+        const props = AConverter.JSONToProps(json);
         assert.deepStrictEqual(props, entity.props);
     });
     it("should convert JSON <-> ValueObject", () => {
-        const converter = createConverter<BValueProps, BValueJSON>(BValue, {
-            a: [prop => prop, json => json],
-            b: [prop => prop, json => json]
-        });
         const value = new BValue({
-            a: 42,
-            b: "b prop"
+            b1: 42,
+            b2: "b prop"
         });
-        const json = converter.toJSON(value);
+        const json = BConverter.toJSON(value);
         assert.deepStrictEqual(json, {
-            a: 42,
-            b: "b prop"
+            b1: 42,
+            b2: "b prop"
         });
-        const value2 = converter.fromJSON(json);
+        const value2 = BConverter.fromJSON(json);
         assert.deepStrictEqual(value, value2);
+    });
+
+    it("should convert by child Converter", () => {
+        const ParentConverter = createConverter<ParentProps, ParentJSON>(ParentEntity, {
+            id: [prop => prop.toValue(), json => new ParentIdentifier(json)],
+            a: AConverter,
+            b: BConverter
+        });
+        const a = new AEntity({
+            id: new AIdentifier("a"),
+            a1: 42,
+            a2: "b prop"
+        });
+        const b = new BValue({
+            b1: 42,
+            b2: "b prop"
+        });
+        const parent = new ParentEntity({
+            id: new ParentIdentifier("parent"),
+            a,
+            b
+        });
+        const parentJSON = ParentConverter.toJSON(parent);
+        assert.deepStrictEqual(parentJSON, {
+            id: "parent",
+            a: {
+                a1: 42,
+                a2: "b prop",
+                id: "a"
+            },
+            b: {
+                b1: 42,
+                b2: "b prop"
+            }
+        });
+        const parent_2 = ParentConverter.fromJSON(parentJSON);
+        assert.ok(parent.equals(parent_2));
+        assert.deepStrictEqual(parent, parent_2);
     });
 });
