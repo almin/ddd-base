c19cef28e3f9285488976c15d29edb112ffe0dd3
azu <azuciao@gmail.com>
2018-10-21 18:59:30 +0900

    chore(Util): remove unused function

--- src/TypeUtil.ts
+++ src/TypeUtil.ts
@@ -1,12 +1,4 @@
 /**
  * Constructor type
  */
-import { EntityLike } from "./EntityLike";
-import { ValueObject } from "./ValueObject";
-
 export type Constructor<T = {}> = new (...args: any[]) => T;
-
-/**
- * Get props type from Entity or ValueObject
- */
-export type GetPropsType<T extends EntityLike<any> | ValueObject<any>> = () => T["props"];
